8d2bae24b1f6370e9fc84caea656d24a78202c6d
klumaks <maxklubkov@gmail.com>
2026-04-24 18:35:18 +0300

    config_update

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,16 +2,14 @@
 /**
  * @fileoverview Константы приложения
  * Содержит глобальные настройки и конфигурацию
- * 
+ *
  * @module src/config/constants
  */
 
 /**
  * Базовый URL для API запросов
  * @constant {string}
- * @default 'http://212.233.99.79:8080'
+ * @default 'http://212.233.99.79:8080/api'
  */
 
-export const API_BASE_URL = 'http://212.233.98.238:8080';
-
-
+export const API_BASE_URL = 'http://212.233.98.238:8080/api';
